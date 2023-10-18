92c564127a7eada3b6d651e1088aa29f8fca70ba
Antoine Savard <antoine.savard@mail.mcgill.ca>
2023-10-18 16:45:05 -0400

    bugs

--- src/mpi_coms.f90
+++ src/mpi_coms.f90
@@ -13,18 +13,23 @@ subroutine broadcasting (num_threads)
     integer, intent(in) :: num_threads
 
     ! openmp variable
-    call mpi_bcast(num_threads, 1, mpi_integer, master, mpi_comm_world)
+    call mpi_bcast(num_threads, 1, mpi_integer,         &
+                    master, mpi_comm_world, ierr)
 
     ! variable broadcast
-    call mpi_bcast(x, n, mpi_double_precision, master, mpi_comm_world)
-    call mpi_bcast(y, n, mpi_double_precision, master, mpi_comm_world)
-    call mpi_bcast(r, n, mpi_double_precision, master, mpi_comm_world)
-    call mpi_bcast(h, n, mpi_double_precision, master, mpi_comm_world)
+    call mpi_bcast(x, n, mpi_double_precision,          &
+                    master, mpi_comm_world, ierr)
+    call mpi_bcast(y, n, mpi_double_precision,          &
+                    master, mpi_comm_world, ierr)
+    call mpi_bcast(r, n, mpi_double_precision,          &
+                    master, mpi_comm_world, ierr)
+    call mpi_bcast(h, n, mpi_double_precision,          &
+                    master, mpi_comm_world, ierr)
     call mpi_bcast(theta, n, mpi_double_precision,      &
-                    master, mpi_comm_world)
+                    master, mpi_comm_world, ierr)
     call mpi_bcast(omega, n, mpi_double_precision,      &
-                    master, mpi_comm_world)
+                    master, mpi_comm_world, ierr)
     call mpi_bcast(bond, n * n, mpi_double_precision,   &
-                    master, mpi_comm_world)
+                    master, mpi_comm_world, ierr)
 
 end subroutine broadcasting
