5e329e2a6eacdc2e5eb632f7ff36d93a6ab6cef1
Antoine Savard <antoine.savard@mail.mcgill.ca>
2023-10-31 10:26:06 -0400

    cleaning

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
+cd generic
 cp SConstruct_generic SConstruct
 cp namelist_generic.nml namelist.nml
 cp input_generic input
 cp input_restart_generic input_restart
 cp start_generic.sh start.sh
+cd ..
