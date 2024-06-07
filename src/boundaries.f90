82f18b2edae93caae97a65a1fbe3e09f620e849e
Antoine Savard <antoine.savard@mail.mcgill.ca>
2024-06-07 17:26:06 -0400

    boundaries

--- src/boundaries.f90
+++ src/boundaries.f90
@@ -8,27 +8,31 @@ subroutine bc_verify (i)
 
     integer, intent(in) :: i
     
-   ! if ( y(i) - r(i) <= 0 ) then
+    if ( y(i) - r(i) <= 0 ) then
 
-    !    v(i) = - v(i)
+        v(i) = - v(i)
+        y(i) = r(i)
 
-    !end if
+    end if
 
     if ( y(i) + r(i) >= ny ) then
 
         v(i) = - v(i)
+        y(i) = ny - r(i)
 
     end if
 
     if ( x(i) - r(i) <= 0 ) then
 
         u(i) = - u(i)
+        x(i) = r(i)
 
     end if
 
     if ( x(i) + r(i) >= nx ) then
 
         u(i) = - u(i)
+        x(i) = nx - r(i)
 
     end if
 
