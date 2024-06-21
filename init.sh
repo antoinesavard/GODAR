575587e766ab00e16091b44b05a3f8e77b41274f
Antoine Savard <antoine.savard@mail.mcgill.ca>
2024-06-21 12:26:29 -0400

    QOL

--- init.sh
+++ init.sh
@@ -5,12 +5,16 @@ mkdir files
 mkdir namelist
 mkdir inputs
 mkdir jobs
+mkdir plots
+mkdir plots/anim
+mkdir plots/plot
 
 cp generic/SConstruct_generic SConstruct
 cp generic/namelist_generic.nml namelist/namelist.nml
 cp generic/input_generic inputs/input
 cp generic/input_restart_generic inputs/input_restart
 cp generic/start_generic.sh jobs/start.sh
-cp generic/args_generic.dat namelist/args.dat
-cp generic/input_init_generic plots/input_init.dat
-cp generic/pyargs_generic.dat utils/pyargs.dat
+cp generic/args_generic.dat utils/args.dat
+cp generic/args_restart_generic.dat utils/args_restart.dat
+cp generic/init_args_generic utils/init_args.dat
+cp generic/video_args_generic.dat utils/video_args.dat
