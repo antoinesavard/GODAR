9668e97a2344708ade35ac5fe21d0f6d58ec40f7
Antoine Savard <antoine.savard@mail.mcgill.ca>
2024-10-01 07:28:48 -0400

    stress and force plots

--- tools/tests/test.py
+++ tools/tests/test.py
@@ -1,10 +1,11 @@
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 from matplotlib.colors import Normalize
 from scipy.spatial import Voronoi, voronoi_plot_2d
 
-sys.path.append("/Users/asavard/Documents/GODAR/")
+sys.path.append("/storage/asavard/DEM/")
 import tools.plotter.figures as tpf
 import tools.utils.files as tuf
 
@@ -45,9 +46,11 @@ filestsigxx = tuf.list_files(output_dir, "tsigxx", expno)
 filestsigyy = tuf.list_files(output_dir, "tsigyy", expno)
 filestsigxy = tuf.list_files(output_dir, "tsigxy", expno)
 filestsigyx = tuf.list_files(output_dir, "tsigyx", expno)
+filestfx = tuf.list_files(output_dir, "tfx", expno)
+filestfy = tuf.list_files(output_dir, "tfy", expno)
 
 # loading the files in memory
-x, y, r, tsigxx, tsigyy, tsigxy, tsigyx = (
+x, y, r, tsigxx, tsigyy, tsigxy, tsigyx, tfx, tfy = (
     tuf.multiload(output_dir, filesx),
     tuf.multiload(output_dir, filesy),
     tuf.multiload(output_dir, filesr),
@@ -55,6 +58,8 @@ x, y, r, tsigxx, tsigyy, tsigxy, tsigyx = (
     tuf.multiload(output_dir, filestsigyy),
     tuf.multiload(output_dir, filestsigxy),
     tuf.multiload(output_dir, filestsigyx),
+    tuf.multiload(output_dir, filestfx),
+    tuf.multiload(output_dir, filestfy),
 )
 
 # compressing the files
@@ -65,7 +70,8 @@ tsigxx = tsigxx[::compression] / sf
 tsigyy = tsigyy[::compression] / sf
 tsigxy = tsigxy[::compression] / sf
 tsigyx = tsigyx[::compression] / sf
-
+tfx = tfx[::compression] / sf
+tfy = tfy[::compression] / sf
 
 # convert it in real stress
 tsigxx = tsigxx / r**2 / np.pi
@@ -81,16 +87,192 @@ tsigxx = tuf.check_dim(tsigxx)
 tsigyy = tuf.check_dim(tsigyy)
 tsigxy = tuf.check_dim(tsigxy)
 tsigyx = tuf.check_dim(tsigyx)
+tfx = tuf.check_dim(tfx)
+tfy = tuf.check_dim(tfy)
 
+sigma = np.stack((tsigxx,tsigyy,tsigxy,tsigyx),axis=-1)
+tf = np.stack((tfx, tfy), axis=-1)
 
 # Generate Voronoi plot
-def plot_stress_field(x, y, stress):
+def plot_stress_field(x, y, stress, time):
+    x = x[time-1]
+    y = y[time-1]
+    stress = stress[time-1]
     points = np.column_stack((x, y))
     vor = Voronoi(points)
 
     # Create a normalization object for consistent color mapping
-    norm = Normalize(vmin=np.min(stress), vmax=np.max(stress))
+    norm = Normalize(vmin=-5e7, vmax=5e7)
+    im = cm.ScalarMappable(norm=norm)
+    
+    # Plot Voronoi tessellation
+    fig, ax = plt.subplots(2,2)
+    voronoi_plot_2d(vor, ax=ax[0,0], show_points=False, show_vertices=False)
+    voronoi_plot_2d(vor, ax=ax[0,1], show_points=False, show_vertices=False)
+    voronoi_plot_2d(vor, ax=ax[1,0], show_points=False, show_vertices=False)
+    voronoi_plot_2d(vor, ax=ax[1,1], show_points=False, show_vertices=False)
 
+    # Color the regions based on normalized stress
+    for point_idx, region_idx in enumerate(vor.point_region):
+        region = vor.regions[region_idx]
+        if not -1 in region and region != []:
+            polygon = [vor.vertices[i] for i in region]
+            value = stress[point_idx,0]
+            # Get the corresponding stress value for this region
+            ax[0,0].fill(*zip(*polygon), color=plt.cm.viridis(norm(value)))
+
+    # Color the regions based on normalized stress
+    for point_idx, region_idx in enumerate(vor.point_region):
+        region = vor.regions[region_idx]
+        if not -1 in region and region != []:
+            polygon = [vor.vertices[i] for i in region]
+            value = stress[point_idx,1]
+            # Get the corresponding stress value for this region
+            ax[1,1].fill(*zip(*polygon), color=plt.cm.viridis(norm(value)))
+
+    # Color the regions based on normalized stress
+    for point_idx, region_idx in enumerate(vor.point_region):
+        region = vor.regions[region_idx]
+        if not -1 in region and region != []:
+            polygon = [vor.vertices[i] for i in region]
+            value = stress[point_idx,2]
+            # Get the corresponding stress value for this region
+            ax[0,1].fill(*zip(*polygon), color=plt.cm.viridis(norm(value)))
+
+
+    # Color the regions based on normalized stress
+    for point_idx, region_idx in enumerate(vor.point_region):
+        region = vor.regions[region_idx]
+        if not -1 in region and region != []:
+            polygon = [vor.vertices[i] for i in region]
+            value = stress[point_idx,3]
+            # Get the corresponding stress value for this region
+            ax[1,0].fill(*zip(*polygon), color=plt.cm.viridis(norm(value)))
+
+            
+    # Scatter plot of particles with the same normalization
+    ax[0,0].scatter(x, y, c=stress[...,0], cmap="viridis", edgecolor="k", norm=norm, alpha=0)
+    ax[0,1].scatter(x, y, c=stress[...,2], cmap="viridis", edgecolor="k", norm=norm, alpha=0)
+    ax[1,0].scatter(x, y, c=stress[...,3], cmap="viridis", edgecolor="k", norm=norm, alpha=0)
+    ax[1,1].scatter(x, y, c=stress[...,1], cmap="viridis", edgecolor="k", norm=norm, alpha=0)
+    fig.colorbar(im, label="Nm$^{-2}$", ax=ax.ravel().tolist())
+    for axis in ax.flat:
+        axis.set_xlim(0, xaxis_limits-13)
+        axis.set_ylim(0, yaxis_limits)
+    ax[0,0].title.set_text("$\sigma_{xx}$")
+    ax[1,1].title.set_text("$\sigma_{yy}$")
+    ax[0,1].title.set_text("$\sigma_{xy}$")
+    ax[1,0].title.set_text("$\sigma_{yx}$")
+    fig.suptitle("$\sigma_{ij}$ at $t=$"+str(time))
+    fig.savefig("../plots/plot/stress_voronoi_{}.pdf".format(time))
+    
+
+# Generate Voronoi plot
+def plot_force_field(x, y, force, time):
+    x = x[time-1]
+    y = y[time-1]
+    force = force[time-1]
+    points = np.column_stack((x, y))
+    vor = Voronoi(points)
+
+    # Create a normalization object for consistent color mapping
+    norm = Normalize(vmin=-5e4, vmax=5e4)
+    im = cm.ScalarMappable(norm=norm)
+    
+    # Plot Voronoi tessellation
+    fig, ax = plt.subplots(1,2)
+    voronoi_plot_2d(vor, ax=ax[0], show_points=False, show_vertices=False)
+    voronoi_plot_2d(vor, ax=ax[1], show_points=False, show_vertices=False)
+
+    # Color the regions based on normalized stress
+    for point_idx, region_idx in enumerate(vor.point_region):
+        region = vor.regions[region_idx]
+        if not -1 in region and region != []:
+            polygon = [vor.vertices[i] for i in region]
+            value = force[point_idx,0]
+            # Get the corresponding stress value for this region
+            ax[0].fill(*zip(*polygon), color=plt.cm.viridis(norm(value)))
+
+    # Color the regions based on normalized stress
+    for point_idx, region_idx in enumerate(vor.point_region):
+        region = vor.regions[region_idx]
+        if not -1 in region and region != []:
+            polygon = [vor.vertices[i] for i in region]
+            value = force[point_idx,1]
+            # Get the corresponding stress value for this region
+            ax[1].fill(*zip(*polygon), color=plt.cm.viridis(norm(value)))
+
+    # Scatter plot of particles with the same normalization
+    ax[0].scatter(x, y, c=force[...,0], cmap="viridis", edgecolor="k", norm=norm, alpha=0)
+    ax[1].scatter(x, y, c=force[...,1], cmap="viridis", edgecolor="k", norm=norm, alpha=0)
+    fig.colorbar(im, label="N", ax=ax.ravel().tolist())
+    for axis in ax.flat:
+        axis.set_xlim(0, xaxis_limits-13)
+        axis.set_ylim(0, yaxis_limits)
+    ax[0].title.set_text("$F_x$")
+    ax[1].title.set_text("$F_y$")
+    fig.suptitle("$F_i$ at $t=$"+str(time))
+    fig.savefig("../plots/plot/force_voronoi_{}.pdf".format(time))
+
+
+def plot_force_field(x, y, force, time):
+    x = x[time-1]
+    y = y[time-1]
+    force = force[time-1]
+    points = np.column_stack((x, y))
+    vor = Voronoi(points)
+
+    # Create a normalization object for consistent color mapping
+    norm = Normalize(vmin=-5e4, vmax=5e4)
+    im = cm.ScalarMappable(norm=norm)
+    
+    # Plot Voronoi tessellation
+    fig, ax = plt.subplots(1,2)
+    voronoi_plot_2d(vor, ax=ax[0], show_points=False, show_vertices=False)
+    voronoi_plot_2d(vor, ax=ax[1], show_points=False, show_vertices=False)
+
+    # Color the regions based on normalized stress
+    for point_idx, region_idx in enumerate(vor.point_region):
+        region = vor.regions[region_idx]
+        if not -1 in region and region != []:
+            polygon = [vor.vertices[i] for i in region]
+            value = force[point_idx,0]
+            # Get the corresponding stress value for this region
+            ax[0].fill(*zip(*polygon), color=plt.cm.viridis(norm(value)))
+
+    # Color the regions based on normalized stress
+    for point_idx, region_idx in enumerate(vor.point_region):
+        region = vor.regions[region_idx]
+        if not -1 in region and region != []:
+            polygon = [vor.vertices[i] for i in region]
+            value = force[point_idx,1]
+            # Get the corresponding stress value for this region
+            ax[1].fill(*zip(*polygon), color=plt.cm.viridis(norm(value)))
+
+    # Scatter plot of particles with the same normalization
+    ax[0].scatter(x, y, c=force[...,0], cmap="viridis", edgecolor="k", norm=norm, alpha=0)
+    ax[1].scatter(x, y, c=force[...,1], cmap="viridis", edgecolor="k", norm=norm, alpha=0)
+    fig.colorbar(im, label="N", ax=ax.ravel().tolist())
+    for axis in ax.flat:
+        axis.set_xlim(0, xaxis_limits-13)
+        axis.set_ylim(0, yaxis_limits)
+    ax[0].title.set_text("$F_x$")
+    ax[1].title.set_text("$F_y$")
+    fig.suptitle("$F_i$ at $t=$"+str(time))
+    fig.savefig("../plots/plot/force_voronoi_{}.pdf".format(time))
+
+def plot_force_quiver(x, y, force, time):
+    x = x[time-1]
+    y = y[time-1]
+    force = force[time-1]
+    force_norm = np.sqrt(force[:,0] ** 2 + force[:,1] ** 2)
+    points = np.column_stack((x, y))
+    vor = Voronoi(points)
+
+    # Create a normalization object for consistent color mapping
+    norm = Normalize(vmin=0, vmax=5e4)
+    im = cm.ScalarMappable(norm=norm)
+    
     # Plot Voronoi tessellation
     fig, ax = plt.subplots()
     voronoi_plot_2d(vor, ax=ax, show_points=False, show_vertices=False)
@@ -100,17 +282,24 @@ def plot_stress_field(x, y, stress):
         region = vor.regions[region_idx]
         if not -1 in region and region != []:
             polygon = [vor.vertices[i] for i in region]
-            value = stress[
-                point_idx
-            ]  # Get the corresponding stress value for this region
-            plt.fill(*zip(*polygon), color=plt.cm.viridis(norm(value)))
+            value = force_norm[point_idx]
+            # Get the corresponding stress value for this region
+            ax.fill(*zip(*polygon), color=plt.cm.viridis(norm(value)))
 
     # Scatter plot of particles with the same normalization
-    sc = plt.scatter(x, y, c=stress, cmap="viridis", edgecolor="k", norm=norm)
-    plt.colorbar(sc, label="Stress")
-    plt.title("Stress Distribution in 2D DEM")
-    plt.show()
+    ax.quiver(x, y, force[:,0], force[:,1])
+    fig.colorbar(im, label="N", ax=ax)
+    ax.set_xlim(0, xaxis_limits-13)
+    ax.set_ylim(0, yaxis_limits)
+    fig.suptitle("$F$ at $t=$"+str(time))
+    fig.savefig("../plots/plot/force_quiver_{}.pdf".format(time))
 
+    
+plot_stress_field(x, y, sigma, 50)
+plot_stress_field(x, y, sigma, 100)
 
-# Example usage for timestep 0
-plot_stress_field(x[-1], y[-1], tsigxx[-1])
+plot_force_field(x, y, tf, 50)
+plot_force_field(x, y ,tf, 100)
+
+plot_force_quiver(x, y, tf, 50)
+plot_force_quiver(x, y, tf, 100)
