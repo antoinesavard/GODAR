c235ef4bd93368d16f9f6eac6d2fe79604c67e58
Antoine Savard <antoine.savard@mail.mcgill.ca>
2024-10-03 13:35:35 +0300

    clean up

--- src/diagnostics.f90
+++ src/diagnostics.f90
@@ -7,7 +7,6 @@ implicit none
     include "CB_const.h"
 	include "CB_bond.h"
 
-    !integer :: i, j
     integer, intent(in) :: tstep
     character(len=4), intent(in) :: expno_str
     character(len=20) :: filex, filey, fileu, filev, filer, fileh, &
@@ -103,60 +102,6 @@ implicit none
                     "stress along y in + x-plane",                     &
                     "Newtons per meter", expno_str, "tsigyx", tstep)
 
-
-    ! ! opening the files
-	! open (10, file = filex, access = 'append', status = 'unknown')
-	! open (11, file = filey, access = 'append', status = 'unknown')
-    ! open (12, file = fileu, access = 'append', status = 'unknown')
-	! open (13, file = filev, access = 'append', status = 'unknown')
-	! open (14, file = filer, access = 'append', status = 'unknown')
-	! open (15, file = fileh, access = 'append', status = 'unknown')
-	! open (16, file = filet, access = 'append', status = 'unknown')
-	! open (17, file = fileo, access = 'append', status = 'unknown')
-	! open (18, file = fileb, access = 'append', status = 'unknown')
-    ! open (19, file = filetfx, access = 'append', status = 'unknown')
-	! open (20, file = filetfy, access = 'append', status = 'unknown')
-    ! open (21, file = filefcx, access = 'append', status = 'unknown')
-	! open (22, file = filefcy, access = 'append', status = 'unknown')
-	! open (23, file = filefbx, access = 'append', status = 'unknown')
-	! open (24, file = filefby, access = 'append', status = 'unknown')
-	! open (25, file = filem, access = 'append', status = 'unknown')
-	! open (26, file = filemc, access = 'append', status = 'unknown')
-	! open (27, file = filemb, access = 'append', status = 'unknown')
-    ! open (28, file = filetsigxx, access = 'append', status = 'unknown')
-    ! open (29, file = filetsigyy, access = 'append', status = 'unknown')
-    ! open (30, file = filetsigxy, access = 'append', status = 'unknown')
-    ! open (31, file = filetsigyx, access = 'append', status = 'unknown')
-
-	! write(10,*) ( x(i),   	i=1, n )
-	! write(11,*) ( y(i),    	i=1, n )
-    ! write(12,*) ( u(i),   	i=1, n )
-	! write(13,*) ( v(i),    	i=1, n )
-	! write(14,*) ( r(i),   	i=1, n )
-	! write(15,*) ( h(i),  	i=1, n )
-	! write(16,*) ( theta(i),	i=1, n )
-	! write(17,*) ( omega(i),	i=1, n )
-	! do i = 1, n
-	! 	write(18,*) ( bond(j,i), j=1, n )
-	! end do
-	! write(18,*)
-    ! write(19,*) ( tfx(i),   i=1, n )
-	! write(20,*) ( tfy(i),   i=1, n )
-    ! write(21,*) ( fcx(i),  	i=1, n )
-	! write(22,*) ( fcy(i),  	i=1, n )
-	! write(23,*) ( fbx(i),   i=1, n )
-	! write(24,*) ( fby(i),  	i=1, n )
-	! write(25,*) ( m(i),	    i=1, n )
-	! write(26,*) ( mc(i),	i=1, n )
-    ! write(27,*) ( mb(i),	i=1, n )
-    ! write(28,*) ( tsigxx(i),	i=1, n )
-    ! write(29,*) ( tsigyy(i),	i=1, n )
-    ! write(30,*) ( tsigxy(i),	i=1, n )
-    ! write(31,*) ( tsigyx(i),	i=1, n )
-
-    ! do i = 10, 31
-    !     close(i)
-    ! end do
 
 end subroutine sea_ice_post
 
