c4ed037269d7249145a21dc7b96dfd40fa6304ac
Antoine Savard <antoine.savard@mail.mcgill.ca>
2024-11-18 18:15:41 +0200

    bonds working with rolling and bc

--- src/bonds.f90
+++ src/bonds.f90
@@ -10,9 +10,8 @@ subroutine bond_forces (j, i)
 	integer, intent(in) :: i, j
 
     double precision :: mbending, mrolling
-    double precision :: krb, knc
-    double precision :: fit
-    double precision :: r_redu, hmin
+    double precision :: krb, gamrb
+    double precision :: m_redu, r_redu, hmin
 
     thetarelb(j,i) = -omegarel(j,i) * dt + thetarelb(j,i)
 
@@ -21,15 +20,14 @@ subroutine bond_forces (j, i)
     deltatb(j,i) = -velt(j,i) * dt + deltatb(j,i)
 
     ! rolling stiffness due to bond
+    m_redu =  mass(i) * mass(j) / ( mass(i) + mass(j) )
     r_redu =  r(i) * r(j) / ( r(i) + r(j) )
 
     hmin   =  min(h(i), h(j))
 
-    knc    =  pi * ec * hmin *                  &
-                fit( deltan(j,i) * r_redu /     &
-                ( 2 * hmin ** 2 ) )
-
-    krb    =  knc * deltat(j,i) ** 2 / 12
+    ! stiffness coefficient
+    krb    =  knb(j,i) * rb(j,i) ** 2 / 3
+    gamrb  = gamma_d * rb(j,i) ** 2 / 3
 
     ! forces are computed from linear elastic material law
     ! F = -kx-cu but x>0 is elongation so the force is supposed
@@ -44,14 +42,15 @@ subroutine bond_forces (j, i)
                 - gamma_d * velt(j,i)
 
 	! moments for bending and twisting motion
-    mbending = ktb(j, i) * ib(j, i) * thetarelb(j,i)
+    mbending = ktb(j, i) * ib(j, i) * thetarelb(j,i) &
+                - gamma_d * omegarel(j,i)
 
     ! moments due to rolling
-    mrolling = krb * thetarelb(j, i)
+    mrolling = krb * thetarelb(j, i) - gamrb * omegarel(j, i)
 
     ! ensures no rolling if moment is too big
-    if ( abs(thetarelb(j, i)) > 2 * (sqrt(3d0) * sigmacb_crit * &
-         hb(j,i) + abs(fcn(j,i))) / knc * deltat(j,i) ) then
+    if ( abs(thetarelb(j, i)) > (sqrt(3d0) * sigmacb_crit * &
+         hb(j,i) + abs(fbn(j,i))) / knb(j,i) / rb(j,i) ) then
             
         mrolling = 0
         !mrolling = abs(fcn(j,i)) * deltat(j,i) / 6 * &
@@ -77,25 +76,28 @@ subroutine bond_breaking (j, i)
 	integer, intent(in) :: i, j
 
 	taub(j, i) = abs(fbt(j, i)) / sb(j, i)
-	sigmatb(j, i) = - fbn(j, i) / sb(j, i) + abs(mbb(j, i)) * 	&
+	sigmatb(j, i) = - abs(fbn(j, i)) / sb(j, i) + abs(mbb(j, i)) * 	&
 					rb(j, i) / ib(j, i)
-	sigmacb(j, i) = fbn(j, i) / sb(j, i) + abs(mbb(j, i)) * 	&
+	sigmacb(j, i) = abs(fbn(j, i)) / sb(j, i) + abs(mbb(j, i)) * 	&
 					rb(j, i) / ib(j, i)
 
 	if ( taub(j, i) .gt. tau_crit * hb(j,i) ) then
 		
+        print*, "1"
 		bond(j, i) = 0
         fbn(j, i)  = 0d0
         fbt(j, i)  = 0d0
 
 	else if ( sigmacb(i, j) .gt. sigmacb_crit * hb(j,i) ) then
 		
+        print*, "2"
 		bond(j, i) = 0
         fbn(j, i)  = 0d0
         fbt(j, i)  = 0d0
 
 	else if ( sigmatb(j, i) .gt. sigmatb_crit * hb(j,i) ) then
 
+        print*, "3"
 		bond(j, i) = 0
         fbn(j, i)  = 0d0
         fbt(j, i)  = 0d0
