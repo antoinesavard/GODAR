7bba0637dbdae8f70c709971833b8d2ef4a24d5e
Antoine Savard <antoine.savard@mail.mcgill.ca>
2024-12-13 00:57:47 -0500

    test file

--- src/test.f90
+++ src/test.f90
@@ -11,33 +11,23 @@ subroutine normal_forces (side, tstep)
     character (*), intent(in) :: side
     integer, intent(in) :: tstep
 
-    double precision :: ftmp, tau
+    double precision :: tau
     integer :: i
     
     ! ~12 seconds is required to resolve the elastic wave travelling 30km
     tau = 50 / dt
 
-    ! forces on the right of the assembly
-    ! forces on the right of the assembly
-    if ( side == "right" ) then
-        ftmp = 0d0!maxval(tfx(n-29:n))
-!        if (tstep*dt < 50d-2) then
-!            if (tstep*dt > 25d-2) then
-                do i = n, n - 9, -1!n, n - 29 , -1
-                    tfy(i) = tfy(i) + ftmp + pfn * tanh( tstep / tau )
-                end do
-!            end if       
-!        end if
-     end if    
-
-    ! no forces on the left of assembly (fixed)
-    ! if ( side == "left" ) then
-    !     do i = n - 30, n - 59 , -1
-    !         tfy(i) = 0d0
-    !         tfx(i) = 0d0
-    !     end do
-    ! end if
-
+    if ( side == "top" ) then
+        do i = n, n - 9, -1
+            tfy(i) = tfy(i) + pfn * tanh( tstep / tau )
+        end do
+    end if 
+    
+    if ( side == "side" ) then
+        do i = n, n - 9, -1
+            tfx(i) = tfx(i) + pfn * tanh( tstep / tau )
+        end do
+    end if
 
 end subroutine normal_forces
 
@@ -56,7 +46,7 @@ subroutine gravity
     angle = 0d0 * 2d0 * pi / 360d0
     fg = mass(1) * 9.81
 
-    do i = 1, 36
+    do i = 1, n
         tfx(i) = tfx(i) + fg * sin(angle)
         tfy(i) = tfy(i) - fg * cos(angle)
     end do
