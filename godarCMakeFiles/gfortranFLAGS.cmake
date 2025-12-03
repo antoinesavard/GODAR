fb10ca97d9ba1a7b02e349496a735886d33bff4a
Antoine Savard <antoine.savard@mail.mcgill.ca>
2025-12-03 13:32:06 +0200

    better deal with warning flags

--- godarCMakeFiles/gfortranFLAGS.cmake
+++ godarCMakeFiles/gfortranFLAGS.cmake
@@ -4,9 +4,9 @@
 message(STATUS "Getting gfortran flags")
 
 # Set flags for all build types
-set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -Wall -Wno-tabs -fPIC -fbackslash")
+set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -fPIC -fbackslash")
 
-set(CMAKE_Fortran_FLAGS_RELEASE "${CMAKE_Fortran_FLAGS} -O3 -funroll-all-loops -finline-functions -ffast-math")
+set(CMAKE_Fortran_FLAGS_RELEASE "${CMAKE_Fortran_FLAGS} -O3 -funroll-all-loops -finline-functions -ffast-math -w")
 
 # on APPLE, it is possible that -static doesn't work
-set(CMAKE_Fortran_FLAGS_DEBUG "${CMAKE_Fortran_FLAGS} -g -Og -static -fbacktrace -fcheck=all")
+set(CMAKE_Fortran_FLAGS_DEBUG "${CMAKE_Fortran_FLAGS} -g -Og -static -fbacktrace -fcheck=all -Wall -Wno-tabs")
